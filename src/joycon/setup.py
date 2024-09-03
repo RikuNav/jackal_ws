1e9def11cb87178ed6423f048212983f187ece43
Paolo Reyes <paolo.alfonso.reyes@gmail.com>
2024-09-02 20:36:10 -0600

    adding joy2bot node

--- src/joycon/setup.py
+++ src/joycon/setup.py
@@ -1,26 +1,28 @@
+import os
+
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'joycon'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='tomatoxplorers',
-    maintainer_email='paolo.alfonso.reyes@gmail.com',
-    description='TODO: Package description',
+    maintainer='Paolo Reyes Ricardo Navarro',
+    maintainer_email='paolo.alfonso.reyes@gmail.com ricardonavarro2003@gmail.com',
+    description='Joycon to velocity message',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-            'joy2bot = joycon.joy2bot:main'
-        ],
+        'console_scripts': [],
     },
 )
