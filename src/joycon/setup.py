a4c25ce30109f1ff63edae42253d62fd31d76638
Paolo Reyes <paolo.alfonso.reyes@gmail.com>
2024-09-02 20:39:21 -0600

    Entry point

--- src/joycon/setup.py
+++ src/joycon/setup.py
@@ -23,6 +23,8 @@ setup(
     license='MIT',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [],
+        'console_scripts': [
+            'joy2bot = joycon.joy2bot:main'
+        ],
     },
 )
